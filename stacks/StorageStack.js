a31136325c3899705b5e4652e98efbd4a898c133
reny <renypacheco08.rp@gmail.com>
2023-03-28 17:42:04 +0800

    Enabling CORS

--- stacks/StorageStack.js
+++ stacks/StorageStack.js
@@ -10,7 +10,16 @@ export function StorageStack({ stack, app }) {
     primaryIndex: { partitionKey: 'userId', sortKey: 'noteId' },
   });
 
-  const bucket = new Bucket(stack, 'Uploads');
+  const bucket = new Bucket(stack, 'Uploads', {
+    cors: [
+      {
+        maxAge: '1 day',
+        allowedOrigins: ['*'],
+        allowedHeaders: ['*'],
+        allowedMethods: ['GET', 'PUT', 'POST', 'DELETE', 'HEAD'],
+      },
+    ],
+  });
 
   return {
     table,
